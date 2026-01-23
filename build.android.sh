b839e0100994a06945f8f494a2e583714ed70cf1
yisier <1340976576@qq.com>
2026-01-24 01:35:49 +0800

    pkg test20

--- build.android.sh
+++ build.android.sh
@@ -1,8 +1,11 @@
-#/bin/bash
+#!/bin/bash
+set -euo pipefail
 
 cd /go
-apt-get install libegl1-mesa-dev libgles2-mesa-dev libx11-dev xorg-dev -y
-go get -u fyne.io/fyne/v2/cmd/fyne fyne.io/fyne/v2
+apt-get update
+apt-get install -y libegl1-mesa-dev libgles2-mesa-dev libx11-dev xorg-dev
+go install fyne.io/fyne/v2/cmd/fyne@latest
+export PATH="$PATH:$(go env GOPATH)/bin"
 #mkdir -p /go/src/fyne.io
 #cd src/fyne.io
 #git clone https://github.com/fyne-io/fyne.git
@@ -30,5 +33,6 @@ rm -rf vendor
 echo "vendor success"
 cd gui/npc
 fyne package -appID org.nps.client -os android -icon ../../docs/logo.png
+test -f npc.apk
 mv npc.apk /app/android_client.apk
 echo "android build success"
