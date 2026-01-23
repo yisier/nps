ce0eed3577466d9bba7a815e7aa5fe9df802c8b9
yisier <1340976576@qq.com>
2026-01-24 01:48:14 +0800

    pkg test21

--- build.android.sh
+++ build.android.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 
 cd /go
+if grep -q "buster" /etc/apt/sources.list; then
+  sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list
+  sed -i 's|http://security.debian.org/debian-security|http://archive.debian.org/debian-security|g' /etc/apt/sources.list
+  echo 'Acquire::Check-Valid-Until "false";' > /etc/apt/apt.conf.d/99no-check-valid
+fi
 apt-get update
 apt-get install -y libegl1-mesa-dev libgles2-mesa-dev libx11-dev xorg-dev
 go install fyne.io/fyne/v2/cmd/fyne@latest
